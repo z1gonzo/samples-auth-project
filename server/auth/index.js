be6a796a50fc901f9bee16b29e004a4e26145053
z1gonzo <goonsky@gmail.com>
2020-04-07 22:44:06 +0200

    server login with res jwt token

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Joi = require('joi');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const db = require('../db/connection');
 
@@ -52,6 +53,54 @@ router.post('/signup', (req, res, next) => {
   } else {
     res.status(422);
     next(result.error);
+  }
+});
+
+function respondError422(res, next) {
+  res.status(422);
+  const error = new Error('Unable to login');
+  next(error);
+}
+
+router.post('/login', (req, res, next) => {
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    users
+      .findOne({
+        username: req.body.username,
+      })
+      .then((user) => {
+        if (user) {
+          bcrypt.compare(req.body.password, user.password).then((result) => {
+            if (result) {
+              const payload = {
+                _id: user._id,
+                username: user.username,
+              };
+              jwt.sign(
+                payload,
+                process.env.TOKEN_SECRET,
+                {
+                  expiresIn: '1d',
+                },
+                (err, token) => {
+                  if (err) {
+                    respondError422(res, next);
+                  } else {
+                    res.json({ token });
+                  }
+                }
+              );
+            } else {
+              respondError422(res, next);
+            }
+          });
+        } else {
+          respondError422(res, next);
+        }
+      });
+  } else {
+    respondError422(res, next);
   }
 });
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const volleyball = require('volleyball');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 const auth = require('./auth/index');
 
